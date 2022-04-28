c35e1ea949bcd09192931204dd2fd6a261265aea
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-04-28 22:28:40 +0300

    chain validity in blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -11,4 +11,27 @@ export default class Blockchain {
         
         return newBlock;
     }    
+
+    isChainValid (){
+
+        //test 1
+        if( JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis) ){
+            return false;
+        }
+
+
+        //test 2
+        for (let i=1;i<this.chain.length;i++){
+            //declare the block and lastblock hashes in r/tion to 'i'th item for postn
+            const block = chain[i];
+            const lastBlock = chain[i-1];
+
+            if( block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block) ){
+                return false;
+            }
+        }
+
+        //if all tests are passed
+        return true;
+    }
 }
