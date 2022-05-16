7cab2c6250cebd0d99b2b7c193ec4404093028d2
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-16 20:42:25 +0300

    Post data :: undefined data in raw by postman (debug)

--- app/index.js
+++ app/index.js
@@ -2,13 +2,23 @@
 
 import Blockchain from "../blockchain/blockchain.js";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const bc = new Blockchain();
-const HTTP_PORT = process.env.HTTP_PORT || '3001';
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+app.use(bodyParser.json());
 
 app.get('/blocks', (req,res) => {
     res.json(bc.chain)
+})
+ 
+app.post('/mine', (req,res) => {
+
+    const block = bc.addBlock(req.body.data)
+    console.log(`New block added: ${block.toString()}`); 
+    res.redirect('/blocks')
 })
 
 app.listen(HTTP_PORT, () => 
