0ae5abab615424cb5338586ebf20445814698709
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-16 20:04:10 +0300

    server running

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
 //api app for user interaction with http req (GET,POST)
+
+import Blockchain from "../blockchain/blockchain.js";
+import express from "express";
+
+const app = express();
+const bc = new Blockchain();
+const HTTP_PORT = process.env.HTTP_PORT || '3001';
+
+app.get('/blocks', (req,res) => {
+    res.json(bc.chain)
+})
+
+app.listen(HTTP_PORT, () => 
+    console.log(`server started at port ${HTTP_PORT}`)
+)
