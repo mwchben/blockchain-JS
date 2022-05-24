00c66300e14d27c9d6356baea00e5ed3c2ecba3e
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-24 20:34:07 +0300

    Clean run sweep

--- dev-bc-test.js
+++ dev-bc-test.js
@@ -2,6 +2,6 @@ import Blockchain from "./blockchain/blockchain.js";
 
 const bc = new Blockchain();
 
-for (let i = 0; i=10; i++) {
-    console.log(bc.addBlock(`good #${$i}`).toString());
+for (let i = 0; i<10; i++) {
+    console.log(bc.addBlock(`good #${i}`).toString());
 }
