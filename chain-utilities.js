a9b04aee9cbe985ca92715018a533862edd6127b
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-25 22:53:32 +0300

    Passed test for keyPAIR

--- chain-utilities.js
+++ chain-utilities.js
@@ -1,11 +1,12 @@
-import { ec as EC } from 'elliptic';
+import pkg from 'elliptic';
+const {ec: EC } = pkg;
 const ec = new EC('secp256k1');
 
  
 class ChainUtil{
     static genKeyPair(){
-        
-        return ec.genKeyPair(); //key = ec.genKeyPair();
+        const key = ec.genKeyPair();
+        return key;
     }
 }
 
