b382a60a8d5a04cd93336d318ad0a843796383f8
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-26 17:11:03 +0300

    ts file and unique ID  generator

--- chain-utilities.js
+++ chain-utilities.js
@@ -1,12 +1,17 @@
 import pkg from 'elliptic';
 const {ec: EC } = pkg;
 const ec = new EC('secp256k1');
+import uuidV1 from 'uuid';
 
  
 class ChainUtil{
     static genKeyPair(){
         const key = ec.genKeyPair();
         return key;
+    }
+
+    static id(){
+        return uuidV1();
     }
 }
 
---
+++ wallet/ts.js
@@ -0,0 +1,27 @@
+import ChainUtil from "../chain-utilities.js";
+
+class Ts {
+    constructor(){
+        this.id = ChainUtil.id();
+        this.input = null;
+        this.outputs = [];
+    }
+
+    static newTs (senderWallet, recepient, amount){
+        const ts = new this();
+
+        if (amount > senderWallet.balance){
+            console.log(`The amount ${amount} exceeds wallet balance`);
+            return;
+        }
+
+        ts.outputs.push(...[
+            { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
+            { amount, address: recepient }
+        ])
+
+        return ts;
+    }
+}
+
+export default Ts;
