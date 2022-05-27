c03868dbcd8aa4a5fc35ea56bfeabb333fefe615
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-27 20:59:38 +0300

    Sign ts

--- chain-utilities.js
+++ chain-utilities.js
@@ -1,3 +1,5 @@
+import sha256 from 'crypto-js/sha256.js';
+
 import pkg from 'elliptic';
 const {ec: EC } = pkg;
 const ec = new EC('secp256k1');
@@ -14,6 +16,11 @@ class ChainUtil{
 
     static id(){
         return uuidv1();
+    }
+
+    //f(x) used in signTs in wallet inputs for the dataHash attr requirement
+    static hash(data){
+        return sha256(JSON.stringify(data)).toString();
     }
 }
 
