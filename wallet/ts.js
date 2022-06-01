1a2b126b07d0ec988389ed12ef385093f6e1a882
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-01 11:19:27 +0300

    sample Ts objects

--- wallet/ts.js
+++ wallet/ts.js
@@ -76,3 +76,62 @@ import ChainUtil from "../chain-utilities.js";
 }
 
 export default Ts;
+
+//.............................. sample code of a Transaction ............................................
+// [
+//     {
+//         "id": "067dd270-e182-11ec-8d36-69481afca498",
+//         "input": {
+//             "timestamp": 1654070929648,
+//             "amount": 500,
+//             "address": "047d0a62e3dc6687327cebb39ce9fade7b901553b6891e632bc1e9bf6f07387b36175427e502bf0ff940cde58a8d1402f0bfa75891bd1fcfca8c39e1d66738ba85",
+//             "signature": {
+//                 "r": "ed2c0f8ae7782c40d871f7abfbecf2aa7ce706c09a33d73ec47cec312286a7e9",
+//                 "s": "2e4250b955f2c35da408318f73226cf1848e1e2e144e71dbbb3b30f31790bf45",
+//                 "recoveryParam": 1
+//             }
+//         },
+//         "outputs": [
+//             {
+//                 "amount": 400,
+//                 "address": "047d0a62e3dc6687327cebb39ce9fade7b901553b6891e632bc1e9bf6f07387b36175427e502bf0ff940cde58a8d1402f0bfa75891bd1fcfca8c39e1d66738ba85"
+//             },
+//             {
+//                 "amount": 50,
+//                 "address": "p4sson-4ddr355"
+//             },
+//             {
+//                 "amount": 50,
+//                 "address": "p4sson-4ddr355"
+//             }
+//         ]
+//     },
+//     {
+//         "id": "127b5240-e183-11ec-ad95-7792b73772ba",
+//         "input": {
+//             "timestamp": 1654071371906,
+//             "amount": 500,
+//             "address": "043112101a8838ea0086418db2faaf66ba90860242e4856c028cf61d16ce09b023340644704b55c349415bd18486475ad6d5ab9dc7b8062c58f05aa347a5f093b6",
+//             "signature": {
+//                 "r": "623a9696ce7d777edefdb561ed07d1f6be8dc48795eb64a70d95848f8d568fec",
+//                 "s": "9f1cf712280f0c11777afd48da1887521e57ac5b5f96c2516e4d7e609f03827c",
+//                 "recoveryParam": 1
+//             }
+//         },
+//         "outputs": [
+//             {
+//                 "amount": 400,
+//                 "address": "043112101a8838ea0086418db2faaf66ba90860242e4856c028cf61d16ce09b023340644704b55c349415bd18486475ad6d5ab9dc7b8062c58f05aa347a5f093b6"
+//             },
+//             {
+//                 "amount": 50,
+//                 "address": "p4sson2-4ddr355"
+//             },
+//             {
+//                 "amount": 50,
+//                 "address": "p4sson2-4ddr355"
+//             }
+//         ]
+//     }
+// ]
+//....................................End of sample code.................................................
