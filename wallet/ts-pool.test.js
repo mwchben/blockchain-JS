825bee6c755471455da70a9c15b572cebaad03ac
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-04 16:58:19 +0300

    Test passes

--- wallet/ts-pool.test.js
+++ wallet/ts-pool.test.js
@@ -9,8 +9,10 @@ describe("Transaction Pool", ()=> {
     beforeEach(()=>{
         tsPool = new TsPool();
         wallet = new Wallet();
-        ts = new Ts.newTs(wallet, 'r4nd-4dr355', 30);
-        tsPool.updateOrAddTs(ts);
+        // ts = new Ts.newTs(wallet, 'r4nd-4dr355', 30);
+        // tsPool.updateOrAddTs(ts);
+        //replaced by createTs()
+        ts = wallet.createTs('r4nd-4dr355',30,tsPool);
     })
 
     //prove tsPool adds ts in the pool
@@ -24,6 +26,32 @@ describe("Transaction Pool", ()=> {
 
         expect(JSON.stringify(tsPool.tsns.find(t => t.id === newTs.id)))
         .not.toEqual( oldTs )
+    })
+
+    describe("mixing valid & corrupt ts", ()=> {
+        let validTsns;
+
+        beforeEach(()=>{
+            validTsns = [...tsPool.tsns];
+        
+            //generate loop for valid ts (odd) and corrupt (even)
+            for (let i = 0; i < 6; i++) {
+                wallet = new Wallet();
+                ts = wallet.createTs('r4nd-4dr355',30,tsPool); 
+                if (i%2 == 0) { //even
+                    ts.input.amount = 89832; //corrupts
+                } else {
+                    validTsns.push(ts)
+                }
+            } 
+        })           
+        
+        it("it shows a difference between valid and corrupt transactions", ()=> {
+            expect(JSON.stringify(tsPool.tsns)).not.toEqual(JSON.stringify(validTsns))
+        })
+        it("it grabs valid transactions", ()=> {
+            expect(tsPool.validTransactions()).toEqual(validTsns)
+        })
     })
  
 })
